81bd24b3a10ac202e2893ca8abfc6e02c994f7bb
John T. Wodder II <git@varonathe.org>
2023-11-01 16:51:38 -0400

    detached-exact

---
+++ git/detached-exact.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -ex
+
+git init -b main
+
+patch -p2 < "$PATCHDIR"/0100-code.diff
+git add .
+git commit -m "Some code"
+
+patch -E -p2 < "$PATCHDIR"/0200-packaged.diff
+rmdir mypackage
+git add -A
+git commit -m "Packaging"
+
+git tag v0.1.0
+
+patch -p2 < "$PATCHDIR"/0300-feature.diff
+git add -A
+git commit -m "Add a feature"
+
+git checkout v0.1.0
