f5511d2c044e9c27e78643f4472fba9af81f828c
John T. Wodder II <git@varonathe.org>
2023-11-02 16:46:45 -0400

    Another sidecar for exact

--- cases/git/exact.py
+++ cases/git/exact.py
@@ -6,7 +6,7 @@ from factory.case import ZipCase
 class TestCase(ZipCase):
     NAME = "exact"
     PATH = Path("repos", "git")
-    EXTRAS = [".json", ".fields.json"]
+    EXTRAS = [".json", ".fields.json", ".exclude.fields.json"]
 
     def build(self) -> None:
         git = self.git()
@@ -18,3 +18,4 @@ class TestCase(ZipCase):
         git.zip()
         self.json({"version": "0.1.0", "next_version": "0.2.0"})
         git.get_info().save(self.asset_path(".fields.json"))
+        git.get_info(exclude=["v*"]).save(self.asset_path(".exclude.fields.json"))
