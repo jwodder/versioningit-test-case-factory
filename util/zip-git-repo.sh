361646a298a26eec553e6ac351935a1657cd580c
John T. Wodder II <git@varonathe.org>
2023-11-01 22:21:14 -0400

    Run `git gc` on Git repos before archiving

---
+++ util/zip-git-repo.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+repo="${1:?Usage: $0 git-repo zipfile}"
+zipfile="${2:?Usage: $0 git-repo zipfile}"
+
+cd "$repo"
+git gc --aggressive --prune=now
+zip --exclude "*.sample" -r "$zipfile" .
